df2a360d044dd545a2fcd0a9c9efcc9cbdb3bee9
Canyons Jordan <canyonsjordan@devcamp.com>
2017-06-20 14:45:35 +0000

    added portfolio via guide

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170619172626) do
+ActiveRecord::Schema.define(version: 20170620143837) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.string "main_image"
+    t.string "text"
+    t.text "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
