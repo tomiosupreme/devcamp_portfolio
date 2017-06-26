c398ba3b8db4ab7dec5c3aac3351f966cea78316
Canyons Jordan <canyonsjordan@devcamp.com>
2017-06-26 16:24:58 +0000

    sad

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,14 +1,14 @@
 class PortfoliosController < ApplicationController
-    def index
+  def index
     @portfolio_items = Portfolio.all
-    end
-    
-    def new
-        @portfolio_items = Portfolio.new
-    end
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
 
   def create
-    @portfolio_items = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
     respond_to do |format|
       if @portfolio_item.save
@@ -18,7 +18,25 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
   def edit
-    @portfolio_items = Portfolio.find(params[:id])
+    @portfolio_item = Portfolio.find(params[:id])
   end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
 end
