ac285d2f02a620928f776167dc893848cf77a43a
Canyons Jordan <canyonsjordan@devcamp.com>
2017-07-05 15:35:35 +0000

    Set Default Values in Rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+3.times do |topic|
+    Topic.create!(
+        title: "topic #{topic}"
+    )
+end
+puts "3 topics created"
+
 10.times do |blog|
     Blog.create!(
         title:"My Blog Post #{blog}",
@@ -14,25 +21,42 @@
         ullam corporis suscipit laboriosam, nisi ut aliquid ex ea
         consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
         quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo 
-        voluptas nulla pariatur?"
-      )
+        voluptas nulla pariatur?",
+        topic_id: Topic.last.id
+    )
 end
 
 puts "10 stupid files created"
 
-5.times do |skill|
-    Skill.create!(
-        title: "rails #{skill}",
-        percent_utilized: 15
-        )
-    end
+# 5.times do |skill|
+#     Skill.create!(
+#         title: "rails #{skill}",
+#         percent_utilized: 15
+#     )
+# end
     
-    puts "5 skills created"
+puts "5 skills created"
     
-    9.times do |portfolio_item|
+8.times do |portfolio_item|
+    Portfolio.create!(
+        title: "porfolio title: #{portfolio_item}", 
+        subtitle: "Ruby on Rails",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit
+            , sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+            irure dolor in reprehenderit in voluptate velit esse cillum
+            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        main_image:"http://via.placeholder.com/600x400" ,
+        thumb_image: "http://via.placeholder.com/300x200"
+    ) 
+end
+    
+     1.times do |portfolio_item|
        Portfolio.create!(
             title: "porfolio title: #{portfolio_item}", 
-            subtitle: "My great service",
+            subtitle: "Angular",
             body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit
             , sed do eiusmod tempor incididunt ut labore et dolore magna
             aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
@@ -41,7 +65,7 @@ puts "10 stupid files created"
             dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
             proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
             main_image:"http://via.placeholder.com/600x400" ,
-            thumb_image: "http://via.placeholder.com/300x200",
+            thumb_image: "http://via.placeholder.com/300x200"
             
             ) 
     end
