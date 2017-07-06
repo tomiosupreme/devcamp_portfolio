f213278c89421534cb9a467f1eb11d353480ac1e
Canyons Jordan <canyonsjordan@devcamp.com>
2017-07-06 14:28:22 +0000

    added placeholder.rb

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,9 @@
 class Skill < ApplicationRecord
     validates_presence_of :title, :percent_utilized
+    
+    after_initialize :set_0defaults
+    
+    def set_defaults 
+        self.badge ||="http://placehold.it/250x2500"
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170703015238) do
+ActiveRecord::Schema.define(version: 20170706141940) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20170703015238) do
     t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
