5cade68ac3be1f19130bfe32a837c1c33cb7ec2f
Canyons Jordan <canyonsjordan@devcamp.com>
2017-07-11 15:25:45 +0000

    dsda

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+         validates_presence_of :name
+         def first_name
+           self.name.split[0]
+         end
+         
+         def last_name
+           self.name.split[1]
+         end
 end
+
