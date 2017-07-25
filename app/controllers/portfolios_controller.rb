a750bd0abd9e7d84722824c6f1c65db2bd289986
Canyons Jordan <canyonsjordan@devcamp.com>
2017-07-25 15:44:40 +0000

    implemented drag and deop feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,6 +6,11 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.order("position ASC")
   end
+  
+  def sort
+  params[:order].each do |key, value|
+    Portfolio.find(value[:id]).update(position: value[:position])
+  end
 
   def angular
     @angular_portfolio_items = Portfolio.angular
@@ -73,7 +78,7 @@ class PortfoliosController < ApplicationController
   end
   
   def set_portfolio_item
-    	@portfolio_item = Portfolio.find(params[:id])
-	end
-
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+end
 end
