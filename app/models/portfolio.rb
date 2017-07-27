4da41b33646ebe0525f85b96e71c3e264e330e9f
Canyons Jordan <canyonsjordan@devcamp.com>
2017-07-27 14:37:28 +0000

    implemented uploader feature for  portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,8 +6,15 @@ class Portfolio < ApplicationRecord
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
+
   def self.angular
     where(subtitle: 'Angular')
+  end
+
+  def self.by_position
+    order("position ASC")
   end
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
